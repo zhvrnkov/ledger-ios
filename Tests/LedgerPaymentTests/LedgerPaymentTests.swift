26feb451d8e240559c005ddb494096921ad3befe
Tublad <tublad92@gmail.com>
2022-06-10 13:44:04 +0600

    rename

--- Tests/LedgerPaymentTests/LedgerPaymentTests.swift
+++ Tests/LedgerPaymentTests/LedgerPaymentTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import LedgerPayment
 
-final class LedgerTests: XCTestCase {
+final class LedgerPaymentTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
