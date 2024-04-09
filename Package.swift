7b6531f23f1e1a7b85e5a07bbe5828f05e214b16
Vlad Zhavoronkov <v.zhavoronkov@ritm.media>
2024-04-09 22:05:26 +0600

    update spm support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,22 @@
-// swift-tools-version:5.3.0
+// swift-tools-version:5.6.0
 
 import PackageDescription
 
-let package = Package(name: "Example",
-                      products: [.library(name: "Example",
-                                          targets: ["Example"])],
-                      dependencies: [],
-                      targets: [.target(name: "Example",
-                                        dependencies: [])])
+let package = Package(
+    name: "Example",
+    products: [
+        .library(
+            name: "Ledger",
+            targets: ["Ledger"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/bizz84/SwiftyStoreKit", exact: "0.16.3"),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", exact: "4.2.2"),
+        .package(url: "https://github.com/MobileNativeFoundation/Kronos", exact: "4.2.1"),
+        .package(url: "https://github.com/zhvrnkov/ion", branch: "master")
+    ],
+    targets: [
+        .target(name: "Ledger", dependencies: ["SwiftyStoreKit", "KeychainAccess", "Kronos", .product(name: "Ion", package: "ion")])
+    ]
+)
